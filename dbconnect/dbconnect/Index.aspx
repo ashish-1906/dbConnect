b81b93c088b6e90df5eadac8d36256b6f9f17e47
Ashish Raturi <50827069+ashish-1906@users.noreply.github.com>
2019-05-20 15:33:46 +0530

    Add files via upload

---
+++ dbconnect/dbconnect/Index.aspx
@@ -0,0 +1,40 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Index.aspx.cs" Inherits="dbconnect.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>webpage</title>
+    <style>
+        #GridView1 {
+            height:300px;
+            width:500px;
+            text-align:center;
+            font-size:16px;
+            
+
+        }
+        body {
+            background-color:#d1cf7f;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <center><h2 style="color:green">Employee Details</h2></center><br />
+            <center><asp:GridView ID="GridView1" runat="server"></asp:GridView></center>
+        <br />
+       <center> <h2 style="color:green">Enter Data</h2><br />
+        <label>Name : </label> <input type="text" runat="server" name="Name" placeholder="Enter" id="Name"/><br /><br />
+        <label>Email : </label> <input type="text"  runat="server" name="Email"  placeholder="Enter" id="Email" /><br /><br />
+        <label>Salary : </label> <input type="text"  runat="server" name="Salary"  placeholder="Enter" id="Salary"/><br /><br />
+        <label>Dept : &nbsp</label> <input type="text"  runat="server" name="Department"  placeholder="Enter" id="Department"/><br /><br /><br />
+        <asp:Button ID="Insert" runat="server" Text="Insert" OnClick="Button_Insert" />&nbsp; &nbsp; 
+        <asp:Button ID="Update" runat="server" Text="Update" OnClick="Button_Update" />&nbsp; &nbsp; 
+        <asp:Button ID="Delete" runat="server" Text="Delete" OnClick="Button_Delete" />&nbsp; 
+        </center>
+    </div>
+    </form>
+</body>
+</html>
---
+++ dbconnect/dbconnect/Index.aspx.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Web.UI.HtmlControls;
+namespace dbconnect
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            SqlConnection cn = new SqlConnection(@"Data Source=192.168.0.61\newsql;Initial Catalog=AIETraining;User ID=AIETrainingAccount;Password=Training@1234");
+            SqlCommand cmd = new SqlCommand("select * from EmployeeDetails order by Name asc", cn); 
+            DataTable dt = new DataTable();
+            dt.Columns.AddRange(new DataColumn[4] { new DataColumn("Name", typeof(string)),
+                                                   new DataColumn("Email", typeof(string)),
+                                                   new DataColumn("Salary",typeof(int)) ,
+                                                    new DataColumn("Department", typeof(string))
+            });
+            cn.Open();
+            using (cmd)
+            {
+                using (SqlDataAdapter sda = new SqlDataAdapter())
+                {
+                    cmd.Connection = cn;
+                    sda.SelectCommand = cmd;
+                    using (dt)
+                    {
+                        sda.Fill(dt);
+                        GridView1.DataSource = dt;
+                        GridView1.DataBind();
+                    }
+                }
+            }
+            cn.Close();
+        }
+        protected void Button_Insert(object sender, EventArgs e)
+        {
+         SqlConnection cnn1 = new SqlConnection(@"Data Source=192.168.0.61\newsql;Initial Catalog=AIETraining;User ID=AIETrainingAccount;Password=Training@1234");
+         cnn1.Open();
+         SqlParameter name = new SqlParameter();
+         name.ParameterName = "@name";
+         name.Value = Page.Request.Form["Name"];
+         SqlParameter email = new SqlParameter();
+         email.ParameterName = "@email";
+         email.Value = Page.Request.Form["Email"];
+         SqlParameter salary = new SqlParameter();
+         salary.ParameterName = "@salary";
+         salary.Value = Page.Request.Form["Salary"];
+         SqlParameter department = new SqlParameter();
+         department.ParameterName = "@dept";
+         department.Value = Page.Request.Form["Department"];
+           // Response.Write(a+b+c+d);
+           
+           SqlCommand cmd1 = new SqlCommand("INSERT INTO EmployeeDetails(Name, Email, Salary, Department)  VALUES (@name,@email,@salary,@dept);", cnn1);
+           cmd1.Parameters.Add(name);
+           cmd1.Parameters.Add(email);
+           cmd1.Parameters.Add(salary);
+           cmd1.Parameters.Add(department);
+           cmd1.ExecuteNonQuery();
+           Page_Load(this,null);
+           cnn1.Close();
+        }
+        protected void Button_Update(object sender, EventArgs e)
+        {
+            SqlConnection cnn3 = new SqlConnection(@"Data Source=192.168.0.61\newsql;Initial Catalog=AIETraining;User ID=AIETrainingAccount;Password=Training@1234");
+            cnn3.Open();
+            SqlParameter name = new SqlParameter();
+            name.ParameterName = "@name";
+            name.Value = Page.Request.Form["Name"];
+            SqlParameter email = new SqlParameter();
+            email.ParameterName = "@email";
+            email.Value = Page.Request.Form["Email"];
+            SqlParameter salary = new SqlParameter();
+            salary.ParameterName = "@salary";
+            salary.Value = Page.Request.Form["Salary"];
+            SqlParameter department = new SqlParameter();
+            department.ParameterName = "@dept";
+            department.Value = Page.Request.Form["Department"];
+          //  Response.Write(a + b + c + d);
+
+            SqlCommand cmd1 = new SqlCommand("Update EmployeeDetails set Name=@name, Email=@email, Salary=@salary, Department=@dept where Name=@name ;", cnn3);
+            cmd1.Parameters.Add(name);
+            cmd1.Parameters.Add(email);
+            cmd1.Parameters.Add(salary);
+            cmd1.Parameters.Add(department);
+            cmd1.ExecuteNonQuery();
+            Page_Load(this, null);
+            cnn3.Close();
+        }
+        protected void Button_Delete(object sender, EventArgs e)
+        {
+            SqlConnection cnn2 = new SqlConnection(@"Data Source=192.168.0.61\newsql;Initial Catalog=AIETraining;User ID=AIETrainingAccount;Password=Training@1234");
+            cnn2.Open();
+            SqlParameter name = new SqlParameter();
+            name.ParameterName = "@name";
+            name.Value = Page.Request.Form["Name"];
+            
+         //   Response.Write(a + b + c + d);
+            SqlCommand cmd3 = new SqlCommand("Delete from EmployeeDetails where Name=@name;", cnn2);
+            cmd3.Parameters.Add(name);
+            cmd3.ExecuteNonQuery();
+            Page_Load(this, null);
+            cnn2.Close();
+        }
+    }
+}
